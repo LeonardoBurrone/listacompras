fb131bd5e7ebcb6590e228aad5b99d7bf854c599
leonardo <leonardo.burrone@matera.com>
2018-12-12 08:46:46 -0200

    Section 3, until lesson 42

---
+++ src/home/list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { faShoppingBasket, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+import './list.css';
+import CustomCard from '../common/customcard';
+import ListFooter from './listfooter';
+import ListItem from './listitem';
+
+const NewList = () => (
+  <CustomCard
+    containerClass='list-container'
+    footer={<ListFooter />}
+  >
+    <div>
+      <p className='title'>Mês</p>
+      <div className='list-card-body'>
+        <ListItem icon={faShoppingBasket} text='1 Item(s) Restante' />
+        <ListItem icon={faCheck} text='2 Item(s) Comprados' />
+      </div>
+    </div>
+  </CustomCard>
+);
+
+export default NewList;
