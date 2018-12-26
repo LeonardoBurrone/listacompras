8ec9ece529e1444370fd025735b192eeb1c375e3
leonardo <leonardo.burrone@matera.com>
2018-12-26 08:55:55 -0200

    Section 3, until lesson 49

--- src/createlist/index.js
+++ src/createlist/index.js
@@ -1,69 +1,32 @@
 import React from 'react';
-
-import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
-import { InputAdornment } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Typography from '@material-ui/core/Typography';
 
 import './list.css';
-
-const units = ['kg', 'lt', 'un'];
+import Form from './form';
+import CustomCard from '../common/customcard';
 
 const CreateList = () => (
   <div className='page-container'>
-    <form className='form-container'>
-      <div className='form-row'>
-        <TextField
-          label="Lista"
-          name='list'
-          value=''
-          onChange={() => {}}
-          required
-        />
-        <Button variant='outlined' color='secondary'>Adicionar</Button>
-      </div>
+    <Form />
 
-      <div className='form-row'>
-        <TextField
-          label='Produto'
-          name='product'
-          value=''
-          onChange={() => {}}
-          required
-        />
-
-        <TextField
-          label='Quantidade'
-          name='quantity'
-          value=''
-          onChange={() => {}}
-          required
-        />
-
-        <TextField
-          select
-          label='Unidade'
-          name='unity'
-          value=''
-          onChange={() => {}}
-          required
-        >
-          {units.map(unit => (
-            <MenuItem key={unit} value={unit} >{unit}</MenuItem>
-          ))}
-        </TextField>
-
-        <TextField
-          label='Preço'
-          name='price'
-          value=''
-          onChange={() => {}}
-          InputProps={{
-            startAdornment: <InputAdornment position='start'>R$</InputAdornment>
-          }}
-        />
-      </div>
-    </form>
+    <div className='list-items-container'>
+      <CustomCard
+        link='#'
+        containerClass='list-item'
+      >
+        <div>
+          <div className='list-item-header'>
+            <Typography variant='subtitle1' component='h2'>Café</Typography>
+            <Checkbox />
+          </div>
+          <div>
+            <Typography component='p'>1 Unidade</Typography>
+            <Typography component='p'>R$ 10.00</Typography>
+          </div>
+        </div>
+      </CustomCard>
+    </div>
   </div>
 );
 
