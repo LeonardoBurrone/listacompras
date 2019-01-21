20a3fec003de4bd1a5946e3fa84125a9a7ff7db3
leonardo <leonardo.burrone@matera.com>
2019-01-21 13:38:53 -0200

    Section 3, until lesson 64

--- src/createList/form.js
+++ src/createList/form.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+import { Creators as FormActions } from '../store/actions/form';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import { InputAdornment } from '@material-ui/core';
@@ -15,6 +18,19 @@ class Form extends Component {
     unit: '',
     price: '',
     showErrors: false,
+  };
+
+  componentDidUpdate(prevProps) {
+    if(this.props.form.action === 'update' && prevProps.form.productToUpdate !== this.props.form.productToUpdate) {
+      const { product, quantity, unit, price } = this.props.form.productToUpdate;
+      this.setState({
+        product,
+        quantity,
+        unit,
+        price,
+        showErrors: false,
+      });
+    }
   }
 
   handleChange = (event) => {
@@ -97,4 +113,10 @@ class Form extends Component {
   }
 }
 
-export default Form;
+const mapStateToProps = state => ({
+  form: state.form,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(FormActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
