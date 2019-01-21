adf92544aad2fdf6154ee7e7f47cf628d2e68b60
leonardo <leonardo.burrone@matera.com>
2019-01-21 13:16:17 -0200

    Section 3, until lesson 63

--- src/createList/listItemFooter.js
+++ src/createList/listItemFooter.js
@@ -1,15 +1,32 @@
-import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Creators as FormActions } from '../store/actions/form';
 
 const ListItemFooter = (props) => (
   <div className='list-item-footer'>
     <div className='list-card-footer-actions'>
-    <FontAwesomeIcon icon={faPen} color='#00b0ff' size='1x' />
-    <FontAwesomeIcon icon={faTrash} color='#e91e63' size='1x' onClick={() => props.deleteProduct(props.item.id)} />
+    <FontAwesomeIcon
+      icon={faPen}
+      color='#00b0ff'
+      size='1x'
+      onClick={() => props.startUpdate(props.item)}
+    />
+    <FontAwesomeIcon
+      icon={faTrash}
+      color='#e91e63'
+      size='1x'
+      onClick={() => props.deleteProduct(props.item.id)}
+    />
     </div>
     <p>Total: R$ {props.item.total}</p>
   </div>
 );
 
-export default ListItemFooter;
+const mapDispatchToProps = dispatch => bindActionCreators(FormActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(ListItemFooter);
