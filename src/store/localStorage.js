7d5e6ad679e1cf24a4dd7ed2d00e38e2f83e0c7f
leonardo <leonardo.burrone@matera.com>
2019-01-28 13:17:43 -0200

    Section 3, until lesson 72

---
+++ src/store/localStorage.js
@@ -0,0 +1,21 @@
+export const loadState = () => {
+  try {
+    const localStorageState = localStorage.getItem('list');
+    if(localStorageState === null) {
+      return undefined;
+    }
+    return JSON.parse(localStorageState);
+  } catch(error) {
+    console.log('Não foi possível recuperar o estado. Erro: ', error);
+    return undefined;
+  }
+}
+
+export const saveState = state => {
+  try {
+    const localStorageState = JSON.stringify(state);
+    localStorage.setItem('list', localStorageState);
+  } catch(error) {
+    console.log('Não foi possível salvar o estado. Erro: ', error);
+  }
+}
